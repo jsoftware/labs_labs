fb1de04a2eb5bf4e4eb89e64a3ec205d7573fc40
Eric Iverson <eiverson@jsoftware.com>
2020-02-21 11:14:40 -0500

    windows dllwrite.ijt

--- examples/dllwrite/build.bat
+++ examples/dllwrite/build.bat
@@ -1,3 +1,4 @@
-Ucall "\Program Files (x86)\Microsoft Visual Studio 12.0\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
+call "\Program Files (x86)\Microsoft Visual Studio 12.0\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
 call nmake -f makefile_win x64=1 clean
 call nmake -f makefile_win x64=1
+call nmake -f makefile_win x64=1 clean
